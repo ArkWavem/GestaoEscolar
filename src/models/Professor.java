d62deb070e858aeb9d89e4bd534f113690ec98dc
ArkWavem <adfelix.oficial@gmail.com>
2025-03-29 12:48:14 -0300

    feat: Gestão Escolar2

--- src/models/Professor.java
+++ src/models/Professor.java
@@ -1,4 +1,37 @@
 package models;
 
 public class Professor {
+    private String nomeProfessor;
+    private String disciplinaProfessor;
+    private int idadeProfessor;
+
+    public Professor(int idadeProfessor) {
+        this.idadeProfessor = idadeProfessor;
+    }
+
+    public Professor(String nomeProfessor, String disciplinaProfessor) {
+        this.nomeProfessor = nomeProfessor;
+        this.disciplinaProfessor = disciplinaProfessor;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public String getDisciplinaProfessor() {
+        return disciplinaProfessor;
+    }
+
+    public int getIdadeProfessor() {
+        return idadeProfessor;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nomeProfessor='" + nomeProfessor + '\'' +
+                ", disciplinaProfessor='" + disciplinaProfessor + '\'' +
+                ", idadeProfessor=" + idadeProfessor +
+                '}';
+    }
 }
